7b22f004d06285e750c438a4bad0f16553695238
Fernando Bernal <bernalsuarezfernando@gmail.com>
2024-08-10 11:26:30 -0400

    Sin cambios

--- event-create-eventin.php
+++ event-create-eventin.php
@@ -10,7 +10,7 @@
  */
 
 // Evitar el acceso directo al archivo
-if ( !defined('ABSPATH') ) {
+if (!defined('ABSPATH')) {
     die;
 }
 
@@ -21,7 +21,8 @@ if ( !defined('ABSPATH') ) {
  * @param array $request Full details about the request.
  */
 
-function filter_post_data( $event, $request ) {
+function filter_post_data($event, $request)
+{
     $event_data = prepare_item_for_database($request);
     $params = [
         'post_title' => $event_data['post_title'],
@@ -42,7 +43,8 @@ add_action('eventin_event_created', 'filter_post_data', 10, 2);
  * @param array $event Event data
  * @param array $request Full details about the request.
  */
-function update_post_data($event, $request) {
+function update_post_data($event, $request)
+{
     $event_data = prepare_item_for_database($request);
     $pod = pods('exposicion', $event->id);
 
@@ -55,17 +57,16 @@ function update_post_data($event, $request) {
     ];
 
     $organizer_names = [];
-    if( isset($event_data['etn_event_organizer']) ){
+    if (isset($event_data['etn_event_organizer'])) {
         foreach ($event_data['etn_event_organizer'] as $organizer_id) {
             $organizer_name = get_the_title($organizer_id);
             $organizer_names[] = $organizer_name;
         }
     }
     $fields_to_save['artistas'] = $organizer_names;
-
-    foreach ($fields_to_save as $field_name => $field_value) {
-        $pod->save($field_name, $field_value, $event->id);
-    }
+    
+    $pod->add($fields_to_save);
+    
 }
 add_action('eventin_event_updated', 'update_post_data', 10, 2);
 
@@ -75,67 +76,44 @@ add_action('eventin_event_updated', 'update_post_data', 10, 2);
  * @param WP_REST_Request $request Request object.
  * @return WP_Error|object $prepared_item
  */
-function prepare_item_for_database( $request ) {
-    $input_data = json_decode( $request->get_body(), true ) ?? [];
-     $validate   = etn_validate( $input_data, [
-        'title'      => [
-            'required',
-        ],
-        'timezone'   => [
-            'required',
-        ],
-        'start_date' => [
-            'required',
-        ],
-        'end_date'   => [
-            'required',
-        ],
-        'start_time' => [
-            'required',
-        ],
-        'end_time'   => [
-            'required',
-        ],
-    ] );
-
-    if ( is_wp_error( $validate ) ) {
-        return $validate;
-    }
+function prepare_item_for_database($request)
+{
+    $input_data = json_decode($request->get_body(), true) ?? [];
 
     $event_data = [];
-    if ( isset( $input_data['title'] ) ) {
-        $event_data['post_title'] = $input_data['title'];            
-    }        
-    
-    if ( isset( $input_data['organizer'] ) ) {
-        $event_data['etn_event_organizer'] = prepare_organizer( $input_data );
+    if (isset($input_data['title'])) {
+        $event_data['post_title'] = $input_data['title'];
     }
 
-    if ( isset( $input_data['start_date'] ) ) {
+    if (isset($input_data['organizer'])) {
+        $event_data['etn_event_organizer'] = prepare_organizer($input_data);
+    }
+
+    if (isset($input_data['start_date'])) {
         $event_data['etn_start_date'] = $input_data['start_date'];
     }
 
-    if ( isset( $input_data['end_date'] ) ) {
+    if (isset($input_data['end_date'])) {
         $event_data['etn_end_date'] = $input_data['end_date'];
     }
 
-    if ( isset( $input_data['start_time'] ) ) {
+    if (isset($input_data['start_time'])) {
         $event_data['etn_start_time'] = $input_data['start_time'];
     }
 
-    if ( isset( $input_data['end_time'] ) ) {
+    if (isset($input_data['end_time'])) {
         $event_data['etn_end_time'] = $input_data['end_time'];
     }
 
-    if ( isset( $input_data['location'] ) ) {
+    if (isset($input_data['location'])) {
         $event_data['etn_event_location'] = $input_data['location'];
     }
 
-    if ( isset( $input_data['categories'] ) ) {
+    if (isset($input_data['categories'])) {
         $event_data['categories'] = $input_data['categories'];
     }
 
-    if ( isset( $input_data['event_banner'] ) ) {
+    if (isset($input_data['event_banner'])) {
         $event_data['event_banner'] = $input_data['event_banner'];
     }
 
@@ -149,11 +127,12 @@ function prepare_item_for_database( $request ) {
  *
  * @return  array
  */
-function prepare_organizer( $args = [] ) {
-    $orgnizer_type    = isset( $args['organizer_type'] ) ? $args['organizer_type'] : '';
-    $organizers       = isset( $args['organizer'] ) ? $args['organizer'] : '';
+function prepare_organizer($args = [])
+{
+    $orgnizer_type = isset($args['organizer_type']) ? $args['organizer_type'] : '';
+    $organizers = isset($args['organizer']) ? $args['organizer'] : '';
 
-    if ( 'single' === $orgnizer_type ) {
+    if ('single' === $orgnizer_type) {
         return $organizers;
     }
 
